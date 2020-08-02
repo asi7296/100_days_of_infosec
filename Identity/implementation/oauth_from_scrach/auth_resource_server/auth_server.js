3afd19a789718030c3f9bf0936c1315ea73fd0f1
asi7296 <ansujanani@gmail.com>
2020-08-03 00:12:14 +0530

    added comments

--- Identity/implementation/oauth_from_scrach/auth_resource_server/auth_server.js
+++ Identity/implementation/oauth_from_scrach/auth_resource_server/auth_server.js
@@ -22,20 +22,19 @@ var app = express();
 app.use(bodyparser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
 
+// given a set of client details that have hit the /auth endpoint,
+// check if they are valid and if a code should be issued
+// this implementation checks for existance of client_id, referer, redirect_uri, 
+// requires response_type == 'code', requries state parameter
 const validate_client = (client_details) => {
-        // check if the client exists
         var known_client = registered_clients[client_details.client_id];
 
         if(known_client == null)
                 return false;
 
         
-        // check if origin is correct
         if (client_details.referer == known_client.referer) {
-                // check if redirect_uri matches
                 if (client_details.redirect_uri == known_client.redirect_uri) {
-                        //check for authorization code flow and scope and state presence
-                        // scope is optional as per RFC, this implementation requires it
                         if (client_details.state && client_details.response_type == 'code') {
                                 return true;
                         }
@@ -44,12 +43,15 @@ const validate_client = (client_details) => {
         
 };
 
+// endpoint that the client will redirect the resource owner to 
+// when the resource owner picks a scope and hits 'Authenticate Me' from the client page
 app.get('/auth',  (req, res) => {
+        // put all request details into one structure
         var client_details = req.query;
         client_details.referer = req.headers.referer;
 
-        // if this returns true, proceed to authenticate user - sending uesrname/password form
-        // also let the user consent to the scope that is being requested
+        // validate the structure - if the client requesting for auth is integrated with this auth server
+        // if allowed, perform the actual authentication with the resource-owner based on said client
         if(validate_client(client_details)) {
                 res.render('authpage', {
                                 client_uri: client_details.referer,
@@ -66,10 +68,11 @@ app.get('/auth',  (req, res) => {
 });
 
 app.post('/consent', (req, res) => {
-        // perform user authentication here, store a session - req.session.username
-        // generate JWT here for the current user based on user ID in session
-        // also generate a code and map it to that JWT for a limited time
-        // send this code back to the client redirect URI
+        // implement a new scope here
+        // that when requested, sends back all details about the user
+        // eg. the roles that the user has across applications and any custom attributes
+        // this may be used across internal applications and services and even for SSO
+        // this token below later is treated as the payload of a JWT
         var token = null;
         if (req.body.selected_scope == 'getallrolesandperms') {
                 token = {
@@ -89,19 +92,33 @@ app.post('/consent', (req, res) => {
                 token = {authorized: true, username: 'username_here_from_auth_server_session', scope: req.body.selected_scope};
         }
         var code = crypto.randomBytes(5).toString('hex');
+        
+        // typically, also add functionality to time the validity of this token
+        // code-token mapping is implemented to prevent CSRF as well as time-out auth flows if they take too long
+        // to prevent vectors such as replay attacks
         code_token_cache[code] = token;
+        // redirect back to the client's registered redirect_uri with the state that they sent over 
+        // and with the auth code that they will come back with to exchange for the actual token later
         res.redirect(req.body.redirect_uri + `?state=${req.body.state}&code=${code}`);
 });
 
+// client applications would POST here
+// with a code that this server sent out earlier after resource-owner authentication
 app.post('/token', (req, res) => {
+        // make sure that the code is still alive and can be exchanged
         if (code_token_cache[req.body.code]) {
                 console.log('sending back token for code', code_token_cache[req.body.code]);
                 
-                // authenticate the client here - client id and client secret
+                // perform client app authentication here, they need to send over the 'client_secret'
                 if (registered_clients[req.body.client_id].client_secret != req.body.client_secret) {
                         res.send('Client failed authentication');
                 }
 
+                // once client has been authenticated, sign a JWT containing the access token
+                // with auth_server's private key. the public key is held by the client and can be used
+                // to check integrity
+                // also remove this code from auth_server cache so that replay attacks are not prevented
+                // ie. one code can be exchanged for one token per resource-owner authentication
                 else {
                         var priv_key = fs.readFileSync('auth_server_priv.pem');
                         var signed_token = jwt.sign(code_token_cache[req.body.code], priv_key, { algorithm: 'RS256'});
--- Identity/implementation/oauth_from_scrach/client/client_server.js
+++ Identity/implementation/oauth_from_scrach/client/client_server.js
@@ -6,12 +6,15 @@ const request = require('request');
 const fs = require('fs');
 const jwt = require('jsonwebtoken');
 
+// Client Registration Details - has to be set-up with the authorization server prior 
 const auth_server_authorize_endpoint = 'http://15.207.117.122:9000/auth';
 const auth_server_token_endpoint = 'http://15.207.117.122:9000/token';
 const client_id = 'client_1';
-const client_secret = 'this_is_client_1_secret';
 const redirect_uri = 'http://15.207.117.122:8443/oauth_callback';
+// keep this a secret, should be stored securely rather than in source
+const client_secret = 'this_is_client_1_secret';
 
+// used later to comply with spec - RFC-6749 - maintaining a state param to avoid CSRF and time flows
 var state_cache = [];
 
 var app = express();
@@ -28,11 +31,19 @@ app.post('/authme', (req, res) => {
         res.redirect( auth_server_authorize_endpoint + params  );
 });
 
+// this endpoint recieves a code and a state
+// checks if state matches the state_cache ie. has a request gone out and is a code awaited?
+// if so, get the code from the query_string, exchange that once again with the auth server for an actual token
+// why is this done? - Code for Token exchange is done on a secure back-channel
 app.get('/oauth_callback', (req, res) => {
+        // make sure that a request has gone out, and that we are actually awaiting a code
+        // and unsolicited responses are not being serviced
         if (!state_cache.includes(req.query.state)) {
                 res.send('State has changed, either the flow took too long or CSRF');
         }
         else {
+                // send a POST request to the authorization server over a secure channel
+                // to the endpoint that takes codes and gives back access tokens
                 token_request_form = {
                         grant_type: 'authorization_code',
                         code: req.query.code,
@@ -41,6 +52,9 @@ app.get('/oauth_callback', (req, res) => {
                         client_secret: client_secret
                 }
 
+                // send the request, if successful, we take the access token out of the body
+                // and remove the state from the cache, ie. the flow is complete
+                // this token can then be used to access the resources it was scoped to that the client consented to
                 request.post({url: auth_server_token_endpoint, form: token_request_form}, (err, httpResponse, httprespbody) => {
                         var auth_server_pub_key = fs.readFileSync('auth_server_pub.pem');
                         var decoded_token = jwt.verify(httprespbody, auth_server_pub_key, (err, decoded) => {
